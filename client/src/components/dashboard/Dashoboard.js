4925841cf82bc12c61bb8cff9e45f78e3d433d3b
tarasbashuk <clk55amg>
2019-10-03 15:37:03 +0300

    first commit

---
+++ client/src/components/dashboard/Dashoboard.js
@@ -0,0 +1,67 @@
+import React, { Fragment, useEffect } from "react"
+import PropTypes from "prop-types"
+import { connect } from "react-redux"
+import {Link} from 'react-router-dom'
+import Spinner from "../layout/Spinner"
+import DashboardActions from './DashboardActions'
+import Experience from './Experience'
+import Education from './Education'
+import { getCurrentProfile, deleteAccount } from "../../actions/profile"
+
+
+const Dashoboard = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: { user },
+  profile: { profile, loading }
+}) => {
+
+  useEffect(() => {
+    getCurrentProfile()
+  }, [getCurrentProfile])
+
+  return loading && loading === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'> Dashboard</h1>
+      <p className='lead'>
+        <i className='fas fa-user'>Welcome {user && user.name}</i>
+      </p>
+      {profile !== null ? (
+        <Fragment>
+          <DashboardActions/>
+          <Experience experience = {profile.experience}/>
+          <Education education = {profile.education}/>
+          <div className="my-2">
+            <button className="btn btn-danger" onClick={()=> deleteAccount()}>
+              <i className="fas fa-user-minus">Delete My Account</i>
+            </button>
+          </div>
+          </Fragment>
+      ) : (
+        <Fragment>
+            <p>You have not seup a profile. Please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link>
+        </Fragment>
+      )}
+    </Fragment>
+  )
+}
+
+Dashoboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  deleteAccount: PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+})
+
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile, deleteAccount }
+)(Dashoboard)
